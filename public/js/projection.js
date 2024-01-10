2972f76f1d98eb4e3f91e22076caee5ebcbfbd64
Alan Ng <137151083+alanNG5@users.noreply.github.com>
2024-01-10 18:30:42 +0800

    adjust and merge js & html

--- public/js/projection.js
+++ public/js/projection.js
@@ -71,55 +71,25 @@ fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&
     console.error('There has been a problem with your fetch operation:', error);
 });
 
+/*
 
+function loop (x,y) {
+    let row = document.createElement('tr');
 
+    let firstCellRow = document.createElement('td');
+    firstCellRow.textContent = x;
+    row.appendChild(firstCellRow);
 
+for (let days of data.weatherForecast) {
+    let item = document.createElement('td');
+    item.textContent = days[y];
+    row.appendChild(item);
+}
+genTable.appendChild(row);
+}
+loop('Date','forecastDate');
+loop('Weekday','week');
+loop('Condition','forecastWeather');
+loop('Rainfall','PSR');
 
-
-
-    // let row1 = document.createElement('tr');
-    // for (let days of data.weatherForecast) {
-    //     let item = document.createElement('td');
-    //     item.textContent = days.forecastDate;
-    //     row1.appendChild(item);
-    // }
-    //     genTable.appendChild(row1);
-
-    // for (let itemKey in data.weatherForecast) {
-    //     let item = data.weatherForecast[itemKey];
-    //     let tr = document.createElement('tr');
-    
-    //     for (let key in item) {
-    //         let td = document.createElement('td');
-    
-    //         td.textContent = item[key];
-    
-    //         tr.appendChild(td);
-    //     }
-    //     genTable.appendChild(tr);
-    //     }
-    // });
-
-    // let row2 = document.createElement('tr');
-    // for (let days of data.weatherForecast) {
-    //     let item = document.createElement('td');
-    //     item.textContent = days.week;
-    //     row2.appendChild(item);
-    // }
-    //     genTable.appendChild(row2);
-
-    // let row3 = document.createElement('tr');
-    // for (let days of data.weatherForecast) {
-    //     let item = document.createElement('td');
-    //     item.textContent = (days.forecastMaxtemp.value + days.forecastMintemp.value)/2;
-    //     row3.appendChild(item);
-    // }
-    //     genTable.appendChild(row3);
-
-    // let row4 = document.createElement('tr');
-    // for (let days of data.weatherForecast) {
-    //     let item = document.createElement('td');
-    //     item.textContent = days.PSR;
-    //     row4.appendChild(item);
-    // }
-    //     genTable.appendChild(row4);
+*/
