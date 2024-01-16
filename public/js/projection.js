e69685877c25d78689ee5c04cd55cfc21abe0cf0
Alan Ng <137151083+alanNG5@users.noreply.github.com>
2024-01-16 18:26:47 +0800

    make example for fetching csv

--- public/js/projection.js
+++ public/js/projection.js
@@ -276,3 +276,42 @@ loop('Condition','forecastWeather');
 loop('Rainfall_Array','PSR');
 
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+onload = fetch('./js/historical_v.csv').then(res => {
+    return res.text();
+}).then(data => {
+    let result = data.split(/\r?\n|\r/).map(e => {
+        return e.split(',');
+    })
+    result.forEach(e => {
+        let m = e.map(e => {
+            return `<td>${e}</td>`;
+        }); // use '.join()' to eliminate ',' and join elements of <td>
+        let ce = document.createElement('tr');
+        for (i=3; i<=8; i++) {
+        ce.innerHTML += m[i];
+        }
+        document.getElementById('fromCSV').appendChild(ce);
+        console.log(m);
+    })
+});
--- services/projection.ts
+++ services/projection.ts
@@ -14,38 +14,3 @@ projectionRouter.get("/general/forecast", async (req, res) => {
         }
       });
 
-        // fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=en')
-
-        // .then(res => {
-        //     if (!res.ok) {
-        //         throw new Error('Network response was not ok ' + res.statusText);
-        //     }
-        //     return res.json();
-        // })
-
-        // .then((data) => {
-        //      return data.json()
-        //     })
-        // .then ((obj) => {console.log(obj)});
-
-
-        //return res.json(data);
-        // .then((callData) => console.log(callData))
-        // let result = data.weatherForecast
-        // console.log(data.weatherForecast)
-        // res.json(result);
-    
-    // .catch(error => {
-    //     console.error('There has been a problem with your fetch operation:', error);
-    // });
-    
-    // catch (error) {
-    //     console.error(error);
-    //     res.status(500).json({ error: "internal service error" });
-
-
-
-
-
-    
-    // })
